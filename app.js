aecff9db77018df5b0e0cc6464edf3b7039975f9
ahmed-tarek <c.tarek2015@gmail.com>
2026-08-01 21:59:30 +0300

    finally moved, added FN to move the snake when down is pressed

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const easyBoxes = {
 
 let snakeBox = { letter: [], number: [] };
 let appleBox = { letter: [], number: [] };
+let lastKeyStroke = "";
+let lastSnakeBox = { letter: [], number: [] };
 
 ////////////////////////////////_____Cached_Element_References___////////////////////////
 
@@ -26,16 +28,9 @@ const easyGrid = document.querySelectorAll(".box");
 
 ////////////////////////////////_____Functions____////////////////////////
 
-// const randomizer = () => {
-//   randomBox.letter = easyBoxes.letters.at(randomNum);
-//   randomBox.number = easyBoxes.numbers.at(randomNum);
-// };
-// randomizer();
 const snakePlace = () => {
   snakeBox.letter = easyBoxes.letters.at(Math.floor(Math.random() * 5));
   snakeBox.number = easyBoxes.numbers.at(Math.floor(Math.random() * 5));
-  console.log(snakeBox);
-
   easyGrid.forEach((box) => {
     if (
       box.classList.contains(snakeBox.letter) &&
@@ -49,7 +44,6 @@ const snakePlace = () => {
 const applePlace = () => {
   appleBox.letter = easyBoxes.letters.at(Math.floor(Math.random() * 5));
   appleBox.number = easyBoxes.numbers.at(Math.floor(Math.random() * 5));
-  console.log(appleBox);
   if (
     snakeBox.letter === appleBox.letter &&
     snakeBox.number === appleBox.number
@@ -57,8 +51,6 @@ const applePlace = () => {
     appleBox.letter = easyBoxes.letters.at(Math.floor(Math.random() * 5));
     appleBox.number = easyBoxes.numbers.at(Math.floor(Math.random() * 5));
   }
-  console.log(appleBox);
-
   easyGrid.forEach((box) => {
     if (
       box.classList.contains(appleBox.letter) &&
@@ -69,21 +61,63 @@ const applePlace = () => {
     }
   });
 };
+const snakeMove = (event) => {
+  lastKeyStroke = event.key;
+  console.log(lastKeyStroke);
+  if (lastKeyStroke === "s" || lastKeyStroke === "ArrowDown") {
+    const downBoxes = easyBoxes.letters;
+    console.log(downBoxes);
+    const snakeMove = setInterval(() => {
+      easyGrid.forEach((box) => {
+        if (
+          box.classList.contains(snakeBox.letter) &&
+          box.classList.contains(snakeBox.number)
+        ) {
+          box.style.backgroundColor = "rgb(144, 250, 130)";
+          box.style.borderRadius = "0%";
+        }
+      });
+      let idx = {
+        letterValue: snakeBox.letter,
+        letterIdx: downBoxes.findIndex((el) => el === snakeBox.letter),
+        number: snakeBox.number,
+      };
+      snakeBox = {
+        letter: downBoxes.at(idx.letterIdx + 1),
+        number: idx.number,
+      };
+      easyGrid.forEach((box) => {
+        if (
+          box.classList.contains(snakeBox.letter) &&
+          box.classList.contains(snakeBox.number)
+        ) {
+          box.style.backgroundColor = "green";
+          box.style.borderRadius = "15%";
+        }
+      });
+    }, 1000);
+    console.log("hi");
 
+    console.log(snakeBox);
+    console.log(easyBoxes.letters.reverse());
+  }
+};
 const runGame = () => {
   snakePlace();
   applePlace();
 };
-runGame();
 const chosen = (event) => console.log("chosen");
 const typed = (event) => console.log(event.key);
 const clicked = (event) => console.log("clicked");
 const render = (event) => {};
-const init = () => {};
+const init = () => {
+  runGame();
+  //   snakeMove();
+};
 ////////////////////////////////_____Event_Listeners_____////////////////////////
 
 for (input of inputs) input.addEventListener("click", chosen);
-keyStroke.addEventListener("keydown", typed);
+keyStroke.addEventListener("keydown", snakeMove);
 for (btn of btns) btn.addEventListener("click", clicked);
 
 ////////////////////////////////_____Initialization_________////////////////////////
