981fe3b64c90adc38137c07e18117bdf4fc80b4b
ahmed-tarek <c.tarek2015@gmail.com>
2026-08-04 10:45:25 +0300

    added organizing comments, also changing snake colors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 ////////////////////////////////_____Constants_____////////////////////////
 
-const gridSize = 40;
+const gridSize = 20;
 const canvaSize = 400;
-const snakeColor = "white";
+const snakeColorOne = "rgb(0, 121, 0)";
+const snakeColorTwo = "rgb(50, 158, 50)";
+const headColor = "rgb(8, 51, 8)";
 const foodColor = "red";
 const up = ["w", "ArrowUp"];
 const down = ["s", "ArrowDown"];
@@ -13,7 +15,7 @@ const right = ["d", "ArrowRight"];
 
 ////////////////////////////////_____Variables(State)_________////////////////////////
 
-let snake = [{ x: 14, y: 10 }];
+let snake = [];
 let food = { x: 15, y: 10 };
 let dx = 0;
 let dy = 0;
@@ -21,7 +23,7 @@ let nextDx = 0;
 let nextDy = 0;
 let score = 0;
 let gameLoop;
-let speed = 500;
+let speed = 300;
 let isPaused = false;
 let isGameOver = false;
 
@@ -29,28 +31,25 @@ let isGameOver = false;
 
 const canvas = document.getElementById("easy-grid");
 const ctx = canvas.getContext("2d");
-const divs = document.querySelectorAll("div");
+
 const inputs = document.querySelectorAll("input");
 const btns = document.querySelectorAll("button");
+
+const pauseBlock = document.querySelector(".pause-block");
+const loseBlock = document.querySelector(".lose-block");
 
 ////////////////////////////////_____Handle_Functions____////////////////////////
 
 const handleClicks = (event) => {
   const btnFinder = event.target.textContent.toLowerCase();
+
+  // Handle restart and playagain buttons
   if (btnFinder.includes("restart") || btnFinder.includes("play again")) {
     newGame();
-    divs.forEach((div) => {
-      if (div.classList.contains("lose")) {
-        div.style.display = "none";
-      }
-    });
-    isPaused = false;
-    divs.forEach((div) => {
-      if (div.classList.contains("pause-block")) {
-        div.style.display = "none";
-      }
-    });
+    loseBlock.style.display = "none";
   }
+
+  // Handle pause/resume buttons
   if (btnFinder.includes("pause") || btnFinder.includes("resume")) {
     pauseResume();
   }
@@ -58,17 +57,27 @@ const handleClicks = (event) => {
 
 const handleKeyPress = (event) => {
   const key = event.key;
+  // Prevent scrolling for arrow keys
+  if (["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight", " "].includes(key)) {
+    event.preventDefault();
+  }
+
+  // Handle the pause key
   if (key === "Escape") {
     pauseResume();
     return;
   }
+
   const isUp = up.includes(key);
   const isDown = down.includes(key);
   const isLeft = left.includes(key);
   const isRight = right.includes(key);
 
-  if (!isUp && !isDown && !isLeft && !isRight) return;
-
+  // Doesn't process gameloop unless it's a movement key
+  // Doesn't change directions while the game is paused
+  // Added nextdx & nextdy to ensure the game renders
+  // only one key at a time to prevent self collision
+  if ((!isUp && !isDown && !isLeft && !isRight) || isPaused) return;
   if (!gameLoop) {
     gameLoop = setInterval(updateGame, speed);
   }
@@ -90,32 +99,31 @@ const handleKeyPress = (event) => {
 const handleInputs = (event) => {
   const id = event.target.id;
   if (id === "slow") {
-    speed = 500;
+    speed = 450;
   } else if (id === "med") {
-    speed = 350;
+    speed = 300;
   } else if (id === "fast") {
-    speed = 200;
+    speed = 150;
   }
 };
 
 ////////////////////////////////_____Functions____////////////////////////
 
 const pauseResume = () => {
+  // Can't pause when the game hasn't started
+  if (!isGameOver && !gameLoop) {
+    return;
+  }
+
+  // the pause logic
+  // Can't pause when the game is over
   if (!isPaused && !isGameOver) {
     isPaused = true;
     clearInterval(gameLoop);
-    divs.forEach((div) => {
-      if (div.classList.contains("pause-block")) {
-        div.style.display = "flex";
-      }
-    });
+    pauseBlock.style.display = "flex";
   } else {
     isPaused = false;
-    divs.forEach((div) => {
-      if (div.classList.contains("pause-block")) {
-        div.style.display = "none";
-      }
-    });
+    pauseBlock.style.display = "none";
     gameLoop = setInterval(updateGame, speed);
   }
 };
@@ -128,25 +136,21 @@ const gameOver = () => {
   //   ctx.font = "30px Arial";
   //   ctx.fillText("Game Over", 120, 130);
   //   ctx.fillText(`Score: ${score}`, 135, 170);
-  divs.forEach((div) => {
-    if (div.classList.contains("lose")) {
-      div.style.display = "flex";
-    }
-  });
+  loseBlock.style.display = "flex";
 };
 
 const updateGame = () => {
   dx = nextDx;
   dy = nextDy;
   const head = { x: snake[0].x + dx, y: snake[0].y + dy };
-  //self collision:
+  //self collision logic
   for (i = 0; i < snake.length; i++) {
     if (snake[i].x === head.x && snake[i].y === head.y) {
       gameOver();
       return;
     }
   }
-  // wall collision:
+  // wall collision logic
   if (
     head.x < 0 ||
     head.x >= canvaSize / gridSize ||
@@ -157,14 +161,11 @@ const updateGame = () => {
     return;
   }
 
+  // Snake eating and movement logic
   snake.unshift(head);
   if (head.x === food.x && head.y === food.y) {
     score += 10;
-    for (part of snake) {
-      do {
-        generateFood();
-      } while (food.x === part.x && food.y === part.y);
-    }
+    generateFood();
   } else {
     snake.pop();
   }
@@ -176,20 +177,32 @@ const updateGame = () => {
 const drawSnake = () => {
   snake.forEach((part, idx) => {
     if (idx === 0) {
-      ctx.fillStyle = "darkgreen";
+      ctx.fillStyle = headColor;
     } else {
-      ctx.fillStyle = snakeColor;
+      if (idx % 2) {
+        ctx.fillStyle = snakeColorOne;
+      } else {
+        ctx.fillStyle = snakeColorTwo;
+      }
     }
     ctx.fillRect(part.x * gridSize, part.y * gridSize, gridSize, gridSize);
   });
 };
 
 const generateFood = () => {
-  food = {
-    x: Math.floor(Math.random() * (canvaSize / gridSize)),
-    y: Math.floor(Math.random() * (canvaSize / gridSize)),
-  };
+  // Ensures the food doesn't get spawned on the snake body
+  isFoodOnSnake = true;
+  while (isFoodOnSnake) {
+    food = {
+      x: Math.floor(Math.random() * (canvaSize / gridSize)),
+      y: Math.floor(Math.random() * (canvaSize / gridSize)),
+    };
+    isFoodOnSnake = snake.some(
+      (part) => part.x === food.x && part.y === food.y,
+    );
+  }
 };
+
 const drawFood = () => {
   ctx.fillStyle = foodColor;
   ctx.fillRect(food.x * gridSize, food.y * gridSize, gridSize, gridSize);
